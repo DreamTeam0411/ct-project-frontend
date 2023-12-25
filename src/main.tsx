7907a62e5f64ac812d0ac527999fdbcc61e94d29
IhorKiy <kiyanichenko@ukr.net>
2023-12-25 20:05:48 +0200

    ‘notFoundPage’

--- src/main.tsx
+++ src/main.tsx
@@ -1,40 +1,49 @@
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import MainPage from './pages/MainPage/Main.tsx'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import MainPage from "./pages/MainPage/Main.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage.tsx";
 import RegistrationPage from "./pages/RegistrationPage/RegistrationPage.tsx";
-import {SuccessRegistrationPage} from "./pages/SuccessRegistrationPage/SuccessRegistrationPage.tsx";
-import {AllServicesPage} from "./pages/AllServicesPage/AllServicesPage.tsx";
-import {ServicePage} from "./pages/ServicePage/ServicePage.tsx";
+import { SuccessRegistrationPage } from "./pages/SuccessRegistrationPage/SuccessRegistrationPage.tsx";
+import { AllServicesPage } from "./pages/AllServicesPage/AllServicesPage.tsx";
+import { ServicePage } from "./pages/ServicePage/ServicePage.tsx";
+import{NotFoundPage}from "./pages/NotFound/NotFound.tsx";
+import { StrictMode } from "react";
 
 const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <MainPage/>
-	},
-	{
-		path: '/login',
-		element: <LoginPage/>
-	},
-	{
-		path: '/registration',
-		element: <RegistrationPage/>
-	},
-	{
-		path: '/success',
-		element: <SuccessRegistrationPage/>
-	},
-	{
-		path: '/services-all',
-		element: <AllServicesPage/>
-	},
-	{
-		path: '/service',
-		element: <ServicePage/>
-	}
-])
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<RouterProvider router={router}/>
-)
+	{
+    path: "/",
+    element: <MainPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/registration",
+    element: <RegistrationPage />,
+  },
+  {
+    path: "/success",
+    element: <SuccessRegistrationPage />,
+  },
+  {
+    path: "/services-all",
+    element: <AllServicesPage />,
+  },
+  {
+    path: "/service",
+    element: <ServicePage />,
+  },
+  {
+    path: "/*",
+    element: <NotFoundPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
