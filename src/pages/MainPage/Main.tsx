e80559aa2a9d756df39326e65f20f91fcc9a839c
Yaroslav Sokolyansky <93289052+Sokolyanskyi@users.noreply.github.com>
2023-12-30 04:37:10 +0200

    Fix: Feature: Delete Categories & FAQ

--- src/pages/MainPage/Main.tsx
+++ src/pages/MainPage/Main.tsx
@@ -1,9 +1,7 @@
 import Header from "../../components/Layout/Header/Header.tsx";
 import Promo from "./Promo/Promo.tsx";
 import Recommendations from "../../components/Layout/Recomandations/Recommendations.tsx";
-import Categories from "../../components/Layout/Categories/Categories.tsx";
 import AboutUs from "./AboutUs/AboutUs.tsx";
-import FAQ from "../../components/Layout/FAQ/FAQ.tsx";
 import SearchByCity from "../../components/Layout/SearchByCity/SearchByCity.tsx";
 import Footer from "../../components/Layout/Footer/Footer.tsx";
 import styles from './Main.module.css'
@@ -40,13 +38,9 @@ function MainPage() {
 				<Header/>
 				<Promo/>
 				<Recommendations/>
-				<Categories/>
 				<AboutUs/>
-				<FAQ/>
 				<SearchByCity/>
 				<Footer/>
-
-
 			</div>
 		)
 	else {
