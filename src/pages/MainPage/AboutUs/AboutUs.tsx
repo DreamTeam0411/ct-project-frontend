389b0a4d333bc62e7e1ca5595328b307b405edf7
IhorKiy <kiyanichenko@ukr.net>
2023-12-31 16:56:07 +0200

    About component

--- src/pages/MainPage/AboutUs/AboutUs.tsx
+++ src/pages/MainPage/AboutUs/AboutUs.tsx
@@ -1,31 +1,24 @@
-import styles from './AboutUs.module.css'
-import useFetchData from "../../../stores/fetchData.tsx";
-import {Button} from "../../../components/UI/buttons/Button/Button.tsx";
+import styles from "./AboutUs.module.css";
 
 const AboutUs = () => {
-	const dataState = useFetchData(state => state.data)
-	return (
-
-		<div className={styles.aboutUsBlock}>
-			{dataState.aboutUsBlock.map((el, index:number) => (
-				<div key={index} className={styles.block}>
-					<div className={styles.aboutUsImage}>
-						<img src={el.image} alt="icon"/>
-					</div>
-					<div className={styles.aboutUsDescription}>
-						<div className={styles.descriptionBold}>{el.title}</div>
-						<div className={styles.descriptionBold}>{el.description}</div>
-						<div className={styles.description}>{el.description}</div>
-						<div className={styles.description}>{el.description}</div>
-						<div className={styles.button}>
-							<Button children={'Детальніше'}/>
-						</div>
-					</div>
-				</div>
-			))}
-
-
-		</div>
-	)
-}
-export default AboutUs
+  return (
+    <div className={styles.aboutUsContainer}>
+      <h2 className={styles.aboutUsTitle}>Про нас</h2>
+      <img src="/aboutUsImg.jpg" alt="women" className={styles.aboutUsImage} />
+      <img src="" alt="" className={styles.aboutUsImageShadow} />
+      <div className={styles.aboutUsPromoBlock}>
+        <h3 className={styles.aboutUsPromo}>
+          BeautyBook - твій гід у світі краси!
+        </h3>
+        <p className={styles.aboutUsText}>
+          Знайди ідеального майстра за кілька кліків. Великий <br/>вибір послуг та
+          професіоналів, зручний пошук - все<br/> це на нашому сайті.
+        </p>
+        <p className={styles.aboutUsText}>
+          Записуйся на процедури швидко та <br/>без зайвих зусиль.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default AboutUs;
