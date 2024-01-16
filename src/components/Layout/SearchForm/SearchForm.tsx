3cee0defb1eac636980fc2b3c2d9628dea56f93c
IhorKiy <kiyanichenko@ukr.net>
2024-01-16 17:03:26 +0200

    searchForm

--- src/components/Layout/SearchForm/SearchForm.tsx
+++ src/components/Layout/SearchForm/SearchForm.tsx
@@ -1,24 +1,26 @@
 import { useState, MouseEvent } from "react";
-
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./SearchForm.module.css";
 
-interface SearchFormProps {
-  
-}
+interface SearchFormProps {}
 
 export const SearchForm: React.FC<SearchFormProps> = () => {
-  const [inputValue, setInputValue] = useState("");
-  const [inputCity, setInputCity] = useState("");
+  const [searchParams] = useSearchParams();
+  const categorie = searchParams.get("categorie");
+  const city = searchParams.get("city");
+
+  const [inputValue, setInputValue] = useState(categorie || "");
+  const [inputCity, setInputCity] = useState(city || "");
   const navigate = useNavigate();
 
-  const addTask = (evt:MouseEvent) => {
-    console.log(typeof evt);
-    
-    if (inputValue) {
+  console.log(categorie, city);
+
+  const addTask = (evt: MouseEvent) => {
+    if (inputValue || inputCity) {
       evt.preventDefault();
       console.log(inputValue);
-      setInputValue("");
+      console.log(inputCity);
+
       navigate(`/services-all?categorie=${inputValue}&city=${inputCity}`);
     } else {
       evt.preventDefault();
@@ -35,7 +37,7 @@ export const SearchForm: React.FC<SearchFormProps> = () => {
           >
             <img src="/Magnifier.svg" alt="" />
           </button>
-          <input
+          {/* <input
             type="text"
             name="services"
             value={inputValue}
@@ -44,23 +46,56 @@ export const SearchForm: React.FC<SearchFormProps> = () => {
             onChange={(evt) => {
               setInputValue(evt.target.value);
             }}
-          ></input>
+            ></input> */}
+          
+          <select
+            name="services"
+            className={styles.searchFormCategorieBox}
+            value={inputValue}
+            onChange={(evt) => {
+              setInputValue(evt.target.value);
+            }}
+          >
+            <option
+              disabled
+              // selected||
+              hidden
+              // className={styles.searchFormSelectDisabled}
+            >
+              Локація
+            </option>
+            <option value="">Всі сервіси</option>
+            <option value="Догляд за нігтями">Догляд за нігтями</option>
+            <option value="Макіяж">Макіяж</option>
+            <option value="Фарбування та зачіска">Фарбування та зачіска</option>
+            <option value="Жіноча стрижка">Жіноча стрижка</option>
+            <option value="Чоловіча стрижка">Чоловіча стрижка</option>
+            <option value="Вії та брови">Вії та брови</option>
+            <option value="Масаж">Масаж</option>
+            <option value="Татуаж">Татуаж</option>
+            <option value="Видалення волосся">Видалення волосся</option>
+            <option value="Косметологія">Косметологія</option>
+          </select>
+
           <div className={styles.searchFormDivLine}></div>
           <div className={styles.searchFormSelect}>
-            <select name="select" className={styles.searchFormSelectBox}
-            onChange={(event) => {
-              setInputCity(event.target.value);
-            }}
+            <select
+              name="select"
+              className={styles.searchFormSelectBox}
+              value={inputCity}
+              onChange={(evt) => {
+                setInputCity(evt.target.value);
+              }}
             >
               <option
-                value=""
                 disabled
                 selected
                 hidden
-                className={styles.searchFormSelectDisabled}
+                // className={styles.searchFormSelectDisabled}
               >
                 Локація
               </option>
+              <option value="">Всі міста</option>
               <option value="Київ">Київ</option>
               <option value="Харків">Харків</option>
               <option value="Одеса">Одеса</option>
