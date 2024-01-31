f90a40574ca75a031a1ca3ed876ff4bd43b7e58a
Yaroslav Sokolyansky <93289052+Sokolyanskyi@users.noreply.github.com>
2024-01-31 15:57:47 +0200

    Fix: Feature: Add ServiceCardDescription

--- src/pages/ServicePage/ServicePage.tsx
+++ src/pages/ServicePage/ServicePage.tsx
@@ -1,12 +1,13 @@
-import styles from './ServicePage.module.css'
+import styles from "./ServicePage.module.css";
 import Header from "../../components/Layout/Header/Header.tsx";
 import Footer from "../../components/Layout/Footer/Footer.tsx";
 import useFetchDataAllMasters from "../../stores/fetchAllMasters.tsx";
 import Loader from "../../components/UI/Loader/Loader..tsx";
-import {useParams} from "react-router-dom";
-import {ServiceCardDescription} from "../AllServicesPage/ServiceCardDescription/ServiceCardDescription.tsx";
+import { useParams } from "react-router-dom";
+import { ServiceCardDescription } from "../AllServicesPage/ServiceCardDescription/ServiceCardDescription.tsx";
 
 export const ServicePage = () => {
+
 	const searchParams = useParams();
 	const searchId: undefined | string | number = searchParams.serviceId
 	const dataState = useFetchDataAllMasters(state => state.data)
@@ -20,7 +21,12 @@ export const ServicePage = () => {
 
 				<div className={styles.wrapper}>
 					{dataState.map((data) => data.id === +searchId ? <ServiceCardDescription key={data.id}
-																							description={data.description} title={data.title} city={data.city} user={data.user} category={data.category} photo={data.photo}
+																							 description={data.description}
+																							 title={data.title}
+																							 city={data.city}
+																							 user={data.user}
+																							 category={data.category}
+																							 photo={data.photo}
 
 					/> : '')}
 				</div>
