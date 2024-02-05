ee73b0a6a17c1eec5a0edd590ca9e080d80598b5
Yaroslav Sokolyansky <93289052+Sokolyanskyi@users.noreply.github.com>
2024-02-05 20:42:10 +0200

    Fix: Feature: add Validations

--- src/components/Stepper/Steps/StepTwo/StepoTwo.tsx
+++ src/components/Stepper/Steps/StepTwo/StepoTwo.tsx
@@ -2,22 +2,42 @@ import React, { useEffect, useState } from "react";
 import { IStepProps } from "../StepOne/StepOne.tsx";
 import styles from "./StepTwo.module.css";
 import useFetchCategories from "../../../../stores/fetchCategories.tsx";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface IFormData {
+  services: string;
+  address: string;
+  description: string;
+}
 
 export const StepTwo: React.FC<IStepProps> = ({ changeActiveStep }) => {
   const fetchDataCategories = useFetchCategories((state) => state.fetchData);
   const dataStateCategories = useFetchCategories((state) => state.data);
   const [categoryState, setCategoryState] = useState("");
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<IFormData>({ mode: "onChange" });
+
   useEffect(() => {
     fetchDataCategories();
   }, []);
+  const submit: SubmitHandler<IFormData> = (data): void => {
+    console.log("Form data submitted:", data);
+    changeActiveStep(3);
+  };
   return (
     <div className={styles.container}>
-      <form>
+      <form onSubmit={handleSubmit(submit)}>
         <div className={styles.categoriesBlock}>
           <label htmlFor="services">Назва послуги</label>
           <select
             name="services"
             className={styles.categories}
+            {...register("services", {
+              required: "Виберіть послугу",
+            })}
             placeholder={"Виберіть послугу"}
             value={categoryState || ""}
             onChange={(evt) => {
@@ -33,19 +53,39 @@ export const StepTwo: React.FC<IStepProps> = ({ changeActiveStep }) => {
               </option>
             ))}
           </select>
+          <div className={styles.error}>
+            {errors?.services && <p>{errors?.services?.message}</p>}
+          </div>
         </div>
         <div className={styles.address}>
           <label htmlFor="address">Адреса обслуговування</label>
-          <input placeholder="Введіть адресу" name="address" />
+          <input
+            placeholder="Введіть адресу"
+            name="address"
+            {...register("address", { required: "Введіть адрессу" })}
+          />
+          <div className={styles.error}>
+            {errors?.address && <p>{errors?.address?.message}</p>}
+          </div>
         </div>
         <div className={styles.description}>
           <label htmlFor="description">Короткий опис</label>
-          <textarea name="description"></textarea>
+          <textarea
+            name="description"
+            {...register("description", {
+              required: false,
+              maxLength: {
+                value: 300,
+                message: "Максимум 300 символів",
+              },
+            })}
+          ></textarea>
+          <div className={styles.error}>
+            {errors?.description && <p>{errors?.description?.message}</p>}
+          </div>
         </div>
 
-        <button type="submit" onClick={() => changeActiveStep(3)}>
-          Продовжити
-        </button>
+        <button type="submit">Продовжити</button>
       </form>
     </div>
   );
