bcc085bce7e0c03b29b304db32e621b605df63d8
BroSok <9riksok@gmail.com>
2024-03-08 02:43:02 +0200

    Fix: fix authorization, fetching data: admin, cities, categories, masters

--- src/components/Layout/Promo/Promo.tsx
+++ src/components/Layout/Promo/Promo.tsx
@@ -1,37 +1,37 @@
 import styles from "./Promo.module.css";
+import {NavLink} from "react-router-dom";
 
 const Promo = () => {
   return (
-    <div className={styles.promoContainer}>
-      <div className={styles.promoLeftBlock}>
-        <p className={styles.promoText1}>Знаходьте</p>
-        <div className={styles.promoBlockText2}>
-          <p className={styles.promoText2}>найкращих</p>
-        </div>
-        <p className={styles.promoText3}>фахівців та замовляйте</p>
-        <div className={styles.promoBlockText4}>
-          <p className={styles.promoText4}>послуги онлайн !</p>
-        </div>
-        <p className={styles.promoText5}>
-          Записуйтеся на сеанс зручно та просто!
-        </p>
-      </div>
-      <div className={styles.promoRightBlock}>
-        <div className={styles.promoFotoSmall}>
-          <img src="/promo1-min.jpg" alt="promo-1" />
-        </div>
+      <div className={styles.promoContainer}>
+        <div className={styles.promoLeftBlock}>
+          <p className={styles.promoText1}>Знаходьте</p>
+          <div className={styles.promoBlockText2}>
+            <p className={styles.promoText2}>найкращих</p>
+          </div>
+          <p className={styles.promoText3}>фахівців та замовляйте</p>
+          <div className={styles.promoBlockText4}>
+            <p className={styles.promoText4}>послуги онлайн !</p>
+          </div>
+          <p className={styles.promoText5}>
+            Записуйтеся на сеанс зручно та просто!
+          </p>
 
-        <div className={styles.promoFotoLarge}>
-          <img src="/promo2-min.jpg" alt="promo-2" />
         </div>
-        <div className={styles.promoFotoLarge}>
-          <img src="/promo3-min.jpg" alt="promo-3" />
+        <div className={styles.promoButtonContainer}>
+            <NavLink to={'/all-services'}>
+                <button className={styles.promoButton}>Записатись <svg xmlns="http://www.w3.org/2000/svg" width="100%"
+                                                                       height="100%"
+                                                                       >
+                    <path fill="white" d="M7 7h8.586L5.293 17.293l1.414 1.414L17 8.414V17h2V5H7v2z"/>
+                </svg>
+                </button>
+            </NavLink>
         </div>
-        <div className={styles.promoFotoSmall}>
-          <img src="/promo4-min.jpg" alt="promo-4" />
-        </div>
+          <div className={styles.promoRightBlock}>
+              <img src="/PromoBanner.jpg" alt="promo-image"/>
+          </div>
       </div>
-    </div>
   );
 };
 export default Promo;
