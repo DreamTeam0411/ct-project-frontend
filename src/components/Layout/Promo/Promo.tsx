c9ff8f1ff5f544908b036dadcd9c7beaaa670f76
IhorKiy <kiyanichenko@ukr.net>
2024-03-25 19:57:41 +0200

    fix

--- src/components/Layout/Promo/Promo.tsx
+++ src/components/Layout/Promo/Promo.tsx
@@ -20,25 +20,25 @@ const Promo = () => {
         </p>
 
         <div className={styles.promoButtonContainer}>
-        <NavLink to={"/all-services"}>
-          <button className={styles.promoButton}>
-            Записатись{" "}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="100%"
-              height="100%"
-              viewBox="0 0 23 23"
-            >
-              <path
-                fill="white"
-                d="M7 7h8.586L5.293 17.293l1.414 1.414L17 8.414V17h2V5H7v2z"
-              />
-            </svg>
-          </button>
-        </NavLink>
+          <NavLink to={"/all-services"}>
+            <button className={styles.promoButton}>
+              Записатись{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="100%"
+                height="100%"
+                viewBox="0 0 23 23"
+              >
+                <path
+                  fill="white"
+                  d="M7 7h8.586L5.293 17.293l1.414 1.414L17 8.414V17h2V5H7v2z"
+                />
+              </svg>
+            </button>
+          </NavLink>
+        </div>
       </div>
-      </div>
-      
+
       <div className={styles.promoRightBlock}>
         <img src={item.image} alt="promo-image" />
       </div>
